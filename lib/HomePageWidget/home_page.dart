9562da065e339d43eed4af817cfbbde4d5bbcf6d
yungNita <yung.nita1306@gmail.com>
2025-05-26 09:40:27 +0700

    update food_card & background in homepage

---
+++ lib/HomePageWidget/home_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'food_card.dart';
+import '../gradient_background.dart'; 
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GradientBackground(
+      child: Scaffold(
+        backgroundColor: Colors.transparent, 
+        body: Center(
+          child: FoodCard(
+            imageUrl: 'assets/food_card/fish_tomato_mix.png',
+            title: 'Fish Tomato \nMix',
+            price: '\$3.00',
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/gradient_background.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class GradientBackground extends StatelessWidget {
+  final Widget child;
+
+  const GradientBackground({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFFCED0FD), // #CED0FD
+            Color(0xFFEADCF4), // #EADCF4
+            Color(0xFFF9DEEA), // #F9DEEA
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          stops: [0.0, 0.49, 1.0],
+        ),
+      ),
+      child: child,
+    );
+  }
+}
